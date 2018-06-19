d32253a1a3bdae8e6bbbb9b9170f978c58e6c19d
Odselmaa <se11d002@gmail.com>
2018-06-20 01:09:36 +0300

    add token

--- controller.py
+++ controller.py
@@ -1,5 +1,3 @@
-from bson import ObjectId
-
 from model import *
 
 
@@ -11,13 +9,11 @@ def get_access_token(token):
 def add_access_token(payload):
     at = AccessToken.objects(user_id=payload['user_id']).first()
     if at:
-        print(at.to_json())
-
-        at.provider=payload['provider']
-        at.token=payload['token']
-        at.created_when=datetime.datetime.fromtimestamp(payload['created_when'])
-        at.expired_when=datetime.datetime.fromtimestamp(payload['expired_when'])
-        at.update()
+        at.update(provider=payload['provider'],
+                         token=payload['token'],
+                         created_when=datetime.datetime.fromtimestamp(payload['created_when']),
+                         expired_when=datetime.datetime.fromtimestamp(payload['expired_when']),
+                         user_id=payload['user_id'])
     else:
         at = AccessToken(provider=payload['provider'],
                          token=payload['token'],
